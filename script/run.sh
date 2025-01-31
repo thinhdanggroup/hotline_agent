7b2a33336e6ae53c376c0d3a1b42db3e86934ce6
thinhdanggroup <dinhluchvc@gmail.com>
2025-01-31 14:13:43 +0700

    rollback build

--- script/run.sh
+++ script/run.sh
@@ -1,5 +1,5 @@
 echo "VITE_API_URL=${VITE_API_URL}" > src/ui/.env
 
 echo "Starting the server... with VITE_API_URL=${VITE_API_URL}"
-# make build
+make build
 poetry run uvicorn src.main:app --reload --host 0.0.0.0 --port ${PORT:-8000}
